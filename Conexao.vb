f5d856298478b1607e83166571450571bbac3a7b
Rafael Viana <rchagas.sdi@gmail.com>
2021-03-02 00:01:14 -0300

    Aula01-módulo-para-conexão-base-de-dados

---
+++ Conexao.vb
@@ -0,0 +1,24 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Conexao
+    Public conexao As New MySqlConnection("server=localhost;userid=root;password='root';database=vb6;")
+    'Função para abri a conexão
+    Sub abrirConexao()
+        If conexao.State = 0 Then
+            conexao.Open()
+            MsgBox("Conectado com Sucesso!")
+
+        End If
+    End Sub
+    'Função para fechar a conexão
+    Sub fecharConexao()
+        If conexao.State = 1 Then
+            conexao.Close()
+            MsgBox("Erro ao conectar!")
+
+        End If
+    End Sub
+
+
+
+End Module
---
+++ Login.vb
@@ -0,0 +1,8 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Login
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        abrirConexao()
+        fecharConexao()
+    End Sub
+End Class
