e3fdfd374c28c245b6bcba15ad57ae38f3f1471b
Rafael Viana <rchagas.sdi@gmail.com>
2021-03-01 23:30:45 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim mysql As MySqlConnection
+        'Variável para conexão com o servidor local
+        mysql.ConnectionString = "server=;userid;password=;database=;"
+
+    End Sub
+End Class
